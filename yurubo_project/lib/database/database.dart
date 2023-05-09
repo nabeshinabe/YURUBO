48c9374d9f7fe9deea91af8ca9746aef8d2e5e18
Takuto Nabeoka <takuto@TakutonoMacBook-Air.local>
2023-05-09 18:21:09 +0900

    Make Database

--- yurubo_project/lib/database/database.dart
+++ yurubo_project/lib/database/database.dart
@@ -1,7 +1,17 @@
-import 'package:supabase/supabase.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:supabase/supabase.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-// Database API 接続用
-final supabase = SupabaseClient(
-  'SUPABASE_URL', // SupabaseのURLを指定
-  'SUPABASE_KEY', // SupabaseのAPIキーを指定
-);
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // .envを読み込めるように設定.
+  await dotenv.load(fileName: '.env');
+  await Supabase.initialize(
+    url: dotenv.get('VAR_URL'), // .envのURLを取得.
+    anonKey: dotenv.get('VAR_ANONKEY'), // .envのanonキーを取得.
+  );
+  // runApp(main());
+  final supabase = Supabase.instance.client;
+}
--- yurubo_project/lib/main.dart
+++ yurubo_project/lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:yurubo/function/loginpage.dart' as loginpage;
+import 'package:yurubo/database/database.dart' as database;
 
 // ここから実験
 void main() {
+  // Database読み出し
+  database.main();
+  
+  // Database読み出した状態で実行
   runApp(const MaterialApp(
     title: 'YURUBO',
     // 最初はログインページから
